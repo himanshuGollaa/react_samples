066fbc392803dcd9ea213aee2cc1ce0bc2486500
Himanshu <himanshu.55421@gmail.com>
2021-05-13 20:39:03 +0530

    furniture design template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,245 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+  
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <header>
+          <div className="header_wrapper">
+            <div className="header_left">
+              <p className="header_title">International artisan & construction material sourcing company</p>
+              <p className="title_description">Discovering special people producing extraordinary products</p>
+              <div className="anchor_links">
+                <a>Our products</a>
+                <a>Our Services</a>
+              </div>
+            </div>
+            <div className="header_right">
+              <img class="responsive" src="https://djzfsf1yiw-flywheel.netdna-ssl.com/wp-content/uploads/2020/01/banner.jpg"/>
+            </div>
+          </div>
+        </header>
+        
+        <section className="white">
+          <div className="container">
+            <div className="first">
+              <img src="http://localhost:3000/watch2.jpg" />
+            </div>
+            <div className="second">
+              <h2 className="header_title">About Us</h2>
+              <p>Adige Design is a Boston-based international artisan and construction material sourcing company
+                with a passion for quality products. Our mission is to uncover special artisans, unique materials
+                and products, and incorporate them in any development, home, or office.
+              </p>
+            </div>
+          </div>
+
+          
+        </section >
+        <section className="black">
+          <div className="section_title">
+            <h1>Exclusive Store</h1>
+            <p className="sub_title">
+            Brought to Boston by Adige Design 
+            </p>
+            
+          </div>
+          <div className="card_wrapper">
+            <div className="card">
+              <div className="figure_wrapper">
+                <figure>
+                  <img src="http://localhost:3000/watch2.jpg" />
+                  <figcaption>Closet System</figcaption>
+                </figure> 
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="figure_wrapper">
+                <figure>
+                  <img src="http://localhost:3000/watch2.jpg" />
+                  <figcaption>Closet System</figcaption>
+                </figure> 
+              </div>
+            </div>
+
+            <div className="card">
+            <div className="figure_wrapper">
+              <figure>
+                <img src="http://localhost:3000/watch2.jpg" />
+                <figcaption>Closet System</figcaption>
+              </figure> 
+            </div>
+          </div>
+          </div>
+        </section>
+        <section className="white">
+        <div className="container">
+            <div className="first">
+              <img src="http://localhost:3000/watch2.jpg" />
+            </div>
+            <div className="second">
+              <h2 className="header_title">About Us</h2>
+              <p>Adige Design is a Boston-based international artisan and construction material sourcing company
+                with a passion for quality products. Our mission is to uncover special artisans, unique materials
+                and products, and incorporate them in any development, home, or office.
+              </p>
+            </div> 
+        </div>
+        </section>
+        <section className="grey">
+          <div className="section_title">
+              <h1>Exclusive Store</h1>
+              <p className="sub_title">
+              Brought to Boston by Adige Design 
+              </p>
+              
+            </div>
+            <div className="card_wrapper">
+                <div className="card">
+                  <div className="figure_wrapper">
+                    <figure>
+                      <img src="http://localhost:3000/project1.jpg" />
+                      <figcaption>Closet System</figcaption>
+                    </figure> 
+                  </div>
+                </div>
+
+                <div className="card">
+                  <div className="figure_wrapper">
+                    <figure>
+                      <img src="http://localhost:3000/project_two.jpg" />
+                      <figcaption>Closet System</figcaption>
+                    </figure> 
+                  </div>
+                </div>
+
+                <div className="card"> 
+                  <div className="figure_wrapper">
+                      <figure>
+                        <img src="http://localhost:3000/project_three.jpg" />
+                        <figcaption>Closet System</figcaption>
+                      </figure> 
+                  </div>
+            </div>
+          </div>    
+        </section>
+        <section className="black">
+        <div className="section_title">
+            <h1>Exclusive Store</h1>
+            <p className="sub_title">
+            Brought to Boston by Adige Design 
+            </p>
+            
+          </div>
+          <div className="card_wrapper">
+            <div className="card">
+              <div className="figure_wrapper">
+                <figure>
+                  <img src="http://localhost:3000/black_one.jpg" />
+                  <figcaption>Closet System</figcaption>
+                </figure> 
+              </div>
+            </div>
+
+            <div className="card">
+              <div className="figure_wrapper">
+                <figure>
+                  <img src="http://localhost:3000/black_two.jpg" />
+                  <figcaption>Closet System</figcaption>
+                </figure> 
+              </div>
+            </div>
+
+            <div className="card">
+            <div className="figure_wrapper">
+              <figure>
+                <img src="http://localhost:3000/black_three.jpg" />
+                <figcaption>Closet System</figcaption>
+              </figure> 
+            </div>
+          </div>
+          </div>
+        </section>
+        <section className="social_media_wrapper">
+          <div className="Social_media">
+            <h1>Instagram</h1>
+          </div>
+          <div className="card_wrapper">
+              <div className="card">
+                <div className="figure_wrapper">
+                  <figure>
+                    <img src="http://localhost:3000/grey_one.jpg" />
+                    <figcaption>Closet System</figcaption>
+                  </figure> 
+                </div>
+              </div>
+
+              <div className="card">
+                <div className="figure_wrapper">
+                  <figure>
+                    <img src="http://localhost:3000/grey_two.jpg" />
+                    <figcaption>Closet System</figcaption>
+                  </figure> 
+                </div>
+              </div>
+
+              <div className="card">
+                <div className="figure_wrapper">
+                  <figure>
+                    <img src="http://localhost:3000/grey_three.jpg" />
+                    <figcaption>Closet System</figcaption>
+                  </figure> 
+                </div>
+              </div>
+
+              <div className="card">
+              <div className="figure_wrapper">
+                <figure>
+                  <img src="http://localhost:3000/watch1.jpg" />
+                  <figcaption>Closet System</figcaption>
+                </figure> 
+              </div>
+            </div>
+          </div>
+        </section>
+        <footer>
+          <div className="footer_heading">
+            Get In Touch
+          </div>
+          <p className="contact_link">Contact us</p>
+          <div className="middle">
+            <div className="margin child1">
+              <p>100 Summer Street, Suite 1600</p>
+              <p>Boston, MA 02110</p>
+            </div>
+            <div className="margin child2">
+              <p>543-54358743</p>
+              <p>email@email.com</p>
+            </div>
+            <div className="margin child3">
+              <p>Privacy Policy</p>
+              <p>Customer Feedback</p>
+              <p>Copy Right</p>
+            </div>
+            <div className="margin child4">
+              <p>copyrightlink</p>
+            </div>
+          </div>
+          <div className="logo">
+            <img src="http://localhost:3000/logo.png" />
+          </div>  
+          {/* <div className="social_media_icons">
+              <a href="#" className="Facebook_icon">
+                <img src="http://localhost:3000/facebook.png" />
+                </a>
+              <a href="#" className="Instagram_icon"></a>
+              <a href="#" className="LinkedIn_icon"></a>
+            </div> */}
+        </footer>
+    </body>
     </div>
   );
 }
